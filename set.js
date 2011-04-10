432e9bb6f1dd93baba9152a086a51e4bbc9a705d
gkatsev <none@none>
2011-04-10 12:23:59 -0400

    Added method stubs for set operations

--- set.js
+++ set.js
@@ -2,21 +2,69 @@ var Set = function(input){
   var set
     , value = null
 
-  this.get = function(prop){
-    return set[prop], prop || Object.keys(set)
+  this.contains = function(prop){
+    throw new Error('not implemented')
   }
 
-  this.add = function(item){
-    set[item] = value
+  this.empty = function(){
+    throw new Error('not implemented')
   }
 
-  this.unique = function(input){
+  this.size = function(){
+    throw new Error('not implemented')
+  }
+
+  this.get = function(){
+    return Object.keys(set)
+  }
+  
+  this.create = function(){
+    throw new Error('not implemented')
+  }
+
+  this.add = function(prop){
+    set[prop] = value
+  }
+
+  this.remove = function(prop){
+    throw new Error('not implemented')
+  }
+
+  this.union = function(iset){
+    throw new Error('not implemented')
+  }
+
+  this.intersect = function(iset){
+    throw new Error('not implemented')
+  }
+
+  this.difference = function(iset){
+    throw new Error('not implemented')
+  }
+
+  this.subset = function(iset){
+    throw new Error('not implemented')
+  }
+
+  this.pop = function(){
+    throw new Error('not implemented')
+  }
+
+  this.find = function(pred){
+    throw new Error('not implemented')
+  }
+
+  this.clear = function(){
+    throw new Error('not implemented')
+  }
+
+  this.unique = function(iset){
     var set = {}
       , i = 0
       , l = input.length
 
     for(; i < l; i++) {
-      set[input[i]] = value
+      set[iset[i]] = value
     }
 
     return set
